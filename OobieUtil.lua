619d27382e35bef8a0d7fd3798e9cc04fa7aa6b6
Scott Stadt <scott.stadt@gmail.com>
2014-02-02 13:47:02 -0600

    Optimized event handling

--- OobieUtil.lua
+++ OobieUtil.lua
@@ -33,6 +33,109 @@ OobieUtil.TimeSinceLastUpdate = 0
 
 local miks = false
 
+-- ------------------------
+-- EVENT ACTIONS
+-- ------------------------
+
+actions = {
+
+	["CHAT_MSG_COMBAT_FACTION_CHANGE"] = function(arg1, arg2)
+		local factionToWatch = nil
+		
+		if string.find(arg1, "Guild") then
+			factionLookUp = GetGuildInfo("player")
+		else
+			factionLookUp = arg1
+		end
+		
+		if UnitLevel("player") == 90 then
+			--Set Faction watched when reputation increases
+			p(GetNumFactions())
+			for factionIndex=1,GetNumFactions() do
+				local factionName = GetFactionInfo(factionIndex)
+				if (string.find(factionLookUp,factionName)) then
+					if not(IsFactionInactive(factionIndex)) then
+						factionToWatch = factionIndex
+					end
+				end
+			end
+			
+			if factionToWatch ~= nil then
+				local _, _, _, _, _, factionValue = GetFactionInfo(factionToWatch)
+			
+				if factionValue < 42000 then
+					OobieUtil_FactionWatchActive = 1
+					OobieUtil.TimeSinceLastUpdate = 0
+					SetWatchedFactionIndex(factionToWatch)
+				end
+			end
+			
+		end
+	end,
+
+	["CHAT_MSG_LOOT"] = function(arg1, arg2)
+		if string.find(arg1, "You receive loot") and string.find(arg1, "cffa335ee") then
+			PlaySoundFile("Interface\\AddOns\\OobieUtil\\Item.ogg")
+		end
+	end,
+
+	["CHAT_MSG_WHISPER"] = function(arg1, arg2)
+		local zoneName = GetRealZoneText()
+		if (arg1=="doobie") then
+			InviteUnit(arg2)
+		end
+	end,
+
+	["DUEL_REQUESTED"] = function(arg1, arg2)
+		CancelDuel()
+	end,
+
+	["MERCHANT_SHOW"] = function(arg1, arg2)
+		if CanMerchantRepair() then
+			local repairCost = GetRepairAllCost()
+			
+			if repairCost > 0 then
+			
+				local repairString = GetCoinText(repairCost," ")
+				
+				if CanGuildBankRepair() and OobieUtil_GuildBankAutoRepair then
+					RepairAllItems(1)
+					repairString = repairString.." (Guild Funds)"
+				else
+					RepairAllItems()
+				end
+				
+				DEFAULT_CHAT_FRAME:AddMessage("Equipment repaired at a cost of "..repairString, 1, 1, 0)
+				
+			end
+		end
+	end,
+
+	["PLAYER_XP_UPDATE"] = function(arg1, arg2)
+		SetWatchedFactionIndex(0)
+	end,
+
+	["TRAINER_SHOW"] = function(arg1, arg2)
+		SetTrainerServiceTypeFilter("unavailable", 0)
+	end,
+
+	["UI_ERROR_MESSAGE"] = function(arg1, arg2)
+		MikSBT.DisplayMessage(arg1,MikSBT.DISPLAYTYPE_NOTIFICATION,false,255,255,0)
+	end,
+
+	["UI_INFO_MESSAGE"] = function(arg1, arg2)
+		if not(string.find(arg1,"not ready")) and not(string.find(arg1,"drink any more")) and not(string.find(arg1,"Another action is in progress")) then
+			MikSBT.DisplayMessage(arg1,MikSBT.DISPLAYTYPE_NOTIFICATION,false,255,0,0)
+		end
+	end,
+
+	["UNIT_SPELLCAST_SUCCEEDED"] = function(arg1, arg2)
+		if (string.find(arg2, "Portal:")) or (string.find(arg2, "Gate")) and (arg1 == "player") then
+			PlaySoundFile("Interface\\AddOns\\OobieUtil\\gate.ogg")
+		end
+	end
+}
+
 -- -------------------------
 -- DISABLE/MOVE BLIZZ STUFF
 -- -------------------------
@@ -48,6 +151,11 @@ end
 
 function OobieUtil_OnLoad()
 
+	for event, action in pairs(actions) do
+		OobieUtil:RegisterEvent(event)
+	end
+
+	--[[
 	OobieUtil:RegisterEvent("CHAT_MSG_COMBAT_FACTION_CHANGE")
 	OobieUtil:RegisterEvent("CHAT_MSG_LOOT")
 	OobieUtil:RegisterEvent("CHAT_MSG_WHISPER")
@@ -58,6 +166,7 @@ function OobieUtil_OnLoad()
 	OobieUtil:RegisterEvent("UI_ERROR_MESSAGE")
 	OobieUtil:RegisterEvent("UI_INFO_MESSAGE")
 	OobieUtil:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
+	]]
 
 	OobieUtil:SetScript("OnEvent", OobieUtil.OnEvent)
 
@@ -86,8 +195,10 @@ end
 	
 function OobieUtil:OnEvent(event, arg1, arg2)
 
+	actions[event](arg1,arg2)
+
 	--local arg1, arg2 = ...
-	
+	--[[
 	if (event=="CHAT_MSG_COMBAT_FACTION_CHANGE") then
 	
 		local factionToWatch = nil
@@ -186,6 +297,7 @@ function OobieUtil:OnEvent(event, arg1, arg2)
 		end
 		
 	end
+	]]
 
 end
 
